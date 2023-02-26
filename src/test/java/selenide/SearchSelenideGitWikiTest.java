4d6d0f53c6cdbdde2934e4069dee676e16b92f8a
funITest <122874700+funITest@users.noreply.github.com>
2023-02-26 17:08:59 +0300

    rewrite 4 lesson for curs java

--- src/test/java/selenide/SearchSelenideGitWikiTest.java
+++ src/test/java/selenide/SearchSelenideGitWikiTest.java
@@ -2,31 +2,23 @@ package selenide;
 
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-
-public class SelenideRepositorySearchSoftAssertions {
+public class SearchSelenideGitWikiTest {
 
     @Test
-    void shouldFindSelenide() {
-
-        // Откройте страницу Selenide в Github
+    void findSelenide() {
         Selenide.open("https://github.com/selenide/selenide");
         // Перейдите в раздел Wiki проекта
         $("#wiki-tab").pressEnter();
         // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $("[href=\"/selenide/selenide/wiki/SoftAssertions\"]").shouldHave(text("SoftAssertions"));
-        //sleep(5000);
+        // Откройте страницу SoftAssertions
         $("[href=\"/selenide/selenide/wiki/SoftAssertions\"]").click();
-        // Откройте страницу SoftAssertions $(".details-reset .Truncate").click();
-        //$(".details-reset").$(" a.Truncate-text").click();  $(withText("ull tex")).click();
-        //Selenide.open("/SoftAssertions");
-        //sleep(5000);
         // На странице SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-        $$(".markdown-body ol li").last().shouldHave(text("JUnit5"));
-        $(".markdown-body ol li:last-child").$("code").exists();
-
+        $$(".markdown-body ol li").last().$("code").shouldHave(text("junit5"));
     }
 }
